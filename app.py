c27db3b025379ff29ed7d8b35ef53ac136325006
Jake Kenneally <jakekenneally@gmail.com>
2023-10-21 10:14:13 -0400

    connect twilio to messages

--- app.py
+++ app.py
@@ -5,23 +5,24 @@ import connexion
 from gevent.pywsgi import WSGIServer
 from flask import Flask, request
 from twilio import twiml
+from messages import messages
 
 
 app = Flask(__name__)
 
+@app.route('/sms', methods=['POST'])
+def sms():
+    message_body = request.form['Body']
+    resp = twiml.Response()
+    resp.message('Your message has been sent to the Upside Down')
+    messages.add_message(message_body)
+    return str(resp)
+
 if __name__ == '__main__':
     stranger.start_client()
+    app.run()
 
     # app = connexion.App(__name__, specification_dir='./swagger/')
     # app.add_api('swagger.yaml', arguments={'title': 'Stranger board'})
-    app.run()
     http_server = WSGIServer(('', 8080), app)
     http_server.serve_forever()
-
-@app.route('/sms', methods=['POST'])
-def sms():
-    number = request.form['From']
-    message_body = request.form['Body']
-    resp = twiml.Response()
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
-    return str(resp)
